701e37775b26624b94c73cf54b290224b47c55fe
Samuel Mulatua <smjn.samuel@gmail.com>
2020-08-26 14:19:38 +0700

    week 1 data types

---
+++ week-1/week1.py
@@ -0,0 +1,61 @@
+nilai = 100
+ipk = 4.0
+
+# Dynamic Typing
+
+# Casting Data Type
+a = 3
+b = float(a)
+c = 4.0
+d = int(c)
+e = str(d)
+f = ""
+f = 'Halo \nhalo'
+f = '''
+Halo
+halo
+halo
+'''
+
+print(b, c, d)
+
+var = 4.6
+res = int(var)
+print(f)
+
+# Variable di Python itu menggunakan snake case
+nama_lengkap_saya = 'Matthew'
+
+print(3+0.5)
+
+x = 10
+x = x+10
+x += 10
+
+a == b
+
+print('nama' + 'saya')
+print('*' * 3)
+
+# Comparison
+
+print("pacil">"paCil")
+
+x = ['pacil', 12, True]
+print(x)
+
+print([1,2,3] > [2,3,4])
+
+# Boolean Operator (True(1) dan False(0))
+
+# AND, OR, NOT
+True & False
+True | False
+~True
+
+True and False
+True or False
+not True
+
+# Another representation of boolean
+print(bool(['']))
