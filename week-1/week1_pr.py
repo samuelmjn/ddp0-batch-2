44701bb7e60a5b4db718f5d53cc5db774c557e3e
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2020-08-29 14:38:23 +0700

    Add PR

---
+++ week-1/week1_pr.py
@@ -0,0 +1,47 @@
+# No 1
+
+print(5 % 4 % 3 % 2 % 1)
+print(((2**4)*3**4)+4/2-1)
+print(((2%4)%1**0)+(5//7-8))
+print(23 < 23.0 and True)
+print(True or 13==14 or 13%3==2)
+
+# No 2
+# *note : pembagian hasilnya pasti float
+#         floor div hasilnya integer
+var1 = 10
+var2 = var1 % 4
+var3 = var2 / 0.5
+var4 = var3**2
+var5 = var4*var1
+print(var5)
+
+# No 3
+# print(1/0)
+
+# No 4
+kalimat_sakti = "kelapa diparut kepala digaruk"
+print(kalimat_sakti[::-2])
+print("kalimat_sakti"[-1:-6:-1])
+
+# No 5
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# Gabisa pake keyword yang udah dipake python 
+
+# $dollar = "uang"
+_ = "underscore nih"
+tUliSAn_raNdom = "mix case"
+# a e s t h e t i c = "a e s t h e t i c"
+# 3telur = "mi goreng"
+_9bolaNaga = "dragonball"
+# if = "elif else
+
+# No 6
+maung = 2019
+if maung + 1 == "2020":
+    print("Selamat datang CSUI 2020")
+if maung % 10 == 9:
+    print("Selamat datang di Fasilkom, CSUI 2020")
